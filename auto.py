da04fc14071073d81e89c5aeb1aefe7e1d5ddb82
Ted-2564 <63999135+Ted-2564@users.noreply.github.com>
2023-07-31 23:42:42 +0800

    Add files via upload
    拉栓用脚本，连续按大写P可以停止

---
+++ auto.py
@@ -0,0 +1,28 @@
+import win32api
+import win32con
+from time import sleep
+import keyboard
+
+def start():
+      sleep(0.5)
+      win32api.keybd_event(17, 0, 0, 0)#ctrl
+      sleep(0.144)
+      win32api.keybd_event(82, 0, 0, 0)#R
+      sleep(0.08)
+      win32api.keybd_event(82, 0, win32con.KEYEVENTF_KEYUP, 0)  # 释放按键R
+      sleep(0.119)
+      win32api.keybd_event(17, 0, win32con.KEYEVENTF_KEYUP, 0)  # 释放按键ctrl
+      sleep(0.86)
+      win32api.keybd_event(82, 0, 0, 0)#R
+      sleep(0.08)
+      win32api.keybd_event(82, 0, win32con.KEYEVENTF_KEYUP, 0)  # 释放按键R
+
+conts=1
+while True:
+      start()
+      if keyboard.is_pressed('P'):
+            break
+      conts+=1
+      print(conts)
+
+
